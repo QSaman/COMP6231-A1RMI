746cbe047403b7900ec1c877894bd05674f7c50a
Saman Saadi <QSaman@users.noreply.github.com>
2017-10-14 02:26:35 -0400

    Supporting multiple processes for campuses.

--- src/comp6231/a1/campus/Bootstrap.java
+++ src/comp6231/a1/campus/Bootstrap.java
@@ -4,16 +4,10 @@
 package comp6231.a1.campus;
 
 import java.io.IOException;
-import java.net.SocketException;
-import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.logging.FileHandler;
-import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
 
 import comp6231.a1.common.LoggerHelper;
 
@@ -24,23 +18,36 @@ import comp6231.a1.common.LoggerHelper;
 public class Bootstrap {
 	public static ArrayList<Campus> campuses = new ArrayList<Campus>();
 	public static boolean init = false;
+	//If you set the following variable to true, run rmiregistry command from your bin directory
+	public final static boolean different_processes = false;
 	
 	public static synchronized void initServers() throws SecurityException, IOException
 	{
 		if (init)
 			return;
 		init = true;
-		Registry registry = LocateRegistry.createRegistry(1099);
-		System.out.println("Java RMI registry created.");
 		String[] campus_names = {"DVL", "KKL", "WST"};
-		int port = 7777;
-		for (String campus_name : campus_names)
+		int[] ports = {7777, 7778, 7779};
+		if (!different_processes)
 		{
+			LocateRegistry.createRegistry(1099);
+			System.out.println("Java RMI registry created.");
+			initServers(campus_names, ports);
+		}
+	}
+	
+	public static synchronized void initServers(String[] campus_names, int[] ports) throws SecurityException, IOException {
+		Registry registry = LocateRegistry.getRegistry();
+		for (int i = 0; i < campus_names.length; ++i)
+		{
+			String campus_name = campus_names[i];
+			int port = ports[i];
 			Logger logger = LoggerHelper.getCampusServerLogger(campus_name);
-			Campus campus = new Campus(campus_name, registry, "127.0.0.1", port++, logger);
+			Campus campus = new Campus(campus_name, registry, "127.0.0.1", port, logger);
 			campus.startRmiServer();
 			campuses.add(campus);
 		}
+		
 	}
 
 	/**
@@ -49,19 +56,16 @@ public class Bootstrap {
 	 * @throws SecurityException 
 	 */
 	public static void main(String[] args) throws SecurityException, IOException {
-//		Logger logger = Logger.getGlobal();
-//		try {
-//			FileHandler fh = new FileHandler("saman.log");
-//			fh.setFormatter(new SimpleFormatter());
-//			logger.addHandler(fh);
-//		} catch (SecurityException | IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		logger.log(Level.INFO, "Hello World from logger");
-//		logger.log(Level.INFO, "Revan was power: " + LoggerHelper.now());
-		
-		initServers();
+		if (args.length == 0)
+			initServers();
+		else if (args.length == 2)
+		{
+			String[] campus_names = new String[1];
+			campus_names[0] = args[0].trim();
+			int[] ports = new int[1];
+			ports[0] = Integer.parseInt(args[1].trim());
+			initServers(campus_names, ports);
+		}
 	}
 
 }
